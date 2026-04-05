61dafa8b5baba98632356369e2465a18cb9b8ad1
Eetu <99178022+E3TU@users.noreply.github.com>
2026-04-05 21:16:03 +0300

    Icon for upload input

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,12 @@
+<script>
+    import Icon from "@iconify/svelte";
+</script>
+
 <div class="container">
     <h1>Go file manager</h1>
     <div class="file-upload">
         <p>Upload a file</p>
+        <Icon class="upload-icon" icon="solar:upload-outline" width="100" height="100" />
     </div>
 </div>
 
@@ -19,6 +24,9 @@
         font-size: 3rem;
     }
     .file-upload{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
         height: 20rem;
         width: 20rem;
         background-color: var(--gray);
@@ -28,5 +36,10 @@
     }
     p{
         color: var(--text-primary);
+        padding: 1rem;
+    }
+    :global(.upload-icon) {
+        color: var(--text-primary);
+        margin-top: 3rem;
     }
 </style>
