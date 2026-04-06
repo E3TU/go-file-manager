e17206e71131aeb970ea06fc2fb2470192443e28
Eetu <99178022+E3TU@users.noreply.github.com>
2026-04-06 20:35:36 +0300

    Navbar component

---
+++ frontend/src/components/Navbar.svelte
@@ -0,0 +1,33 @@
+<nav class="navbar">
+    <div class="logo">Go file manager</div>
+    <ul>
+        <li><a class="logout">Log out</a></li>
+    </ul>
+</nav>
+
+<style>
+    .navbar{
+        display: flex;
+        width: 100%;
+        height: 6rem;
+        background-color: var(--gray);
+        justify-content: space-between;
+        align-items: center;
+        color: var(--text-primary);
+        box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+    }
+    .logo{
+        margin-left: 10rem;
+        font-size: 2rem;
+    }
+    ul{
+        list-style: none;
+    }
+    a{
+        text-decoration: none;
+    }
+    .logout{
+        margin-right: 5rem;
+        font-size: 1.25rem;
+    }
+</style>
