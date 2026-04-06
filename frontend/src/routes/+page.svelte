b598f3307333f254a1da49024316db5072505302
Eetu <99178022+E3TU@users.noreply.github.com>
2026-04-06 20:35:58 +0300

    Drag and drop the files or choose using btn

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,45 +1,121 @@
-<script>
-    import Icon from "@iconify/svelte";
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	import Navbar from '../components/Navbar.svelte';
+
+	// let fileInput: any;
+
+	let files = $state<File[]>([]);
+
+	let isDragging = $state(false);
+	let inputEl: HTMLInputElement | null = null;
+
+	function onDrop(e: DragEvent) {
+		e.preventDefault();
+		isDragging = false;
+		if (e.dataTransfer?.files?.length) {
+			files = [...files, ...Array.from(e.dataTransfer.files)];
+		}
+	}
+
+	function onDragOver(e: DragEvent) {
+		e.preventDefault();
+		isDragging = true;
+	}
+
+	function onDragLeave() {
+		isDragging = false;
+	}
+
+	function openFileManager() {
+		inputEl?.click();
+	}
+
+    function onInputChange(e: Event) {
+        const target = e.target as HTMLInputElement;
+        if (target.files?.length) {
+            files = [...files, ...Array.from(target.files)];
+            target.value = "";
+        }
+    }
 </script>
 
+<Navbar />
 <div class="container">
-    <h1>Go file manager</h1>
-    <div class="file-upload">
-        <p>Upload a file</p>
-        <Icon class="upload-icon" icon="solar:upload-outline" width="100" height="100" />
-    </div>
+	<!-- <h1>Go file manager</h1> -->
+	<div class="file-upload">
+		<div
+			class:dragging={isDragging}
+			ondrop={onDrop}
+			ondragover={onDragOver}
+			ondragleave={onDragLeave}
+		>
+			<Icon class="upload-icon" icon="solar:upload-outline" width="64" height="64" />
+			<p>Drag and drop files here to upload<br /> or</p>
+			<button onclick={openFileManager} class="choose-file-btn">Choose file</button>
+			<input onchange={onInputChange} bind:this={inputEl} class="file-input" type="file" multiple />
+		</div>
+	</div>
+	<!-- <ul>
+		{#each files as file}
+			<li>{file.name} ({file.size} bytes)</li>
+		{/each}
+	</ul> -->
 </div>
 
 <style>
-    .container{
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        height: 100vh;
-        width: 100%;
-    }
-    h1{
-        color: var(--text-primary);
-        margin-top: 5rem;
-        font-size: 3rem;
-    }
-    .file-upload{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        height: 20rem;
-        width: 20rem;
-        background-color: var(--gray);
-        box-shadow: rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
-        border-radius: 16px;
-        margin-top: 2rem;
-    }
-    p{
-        color: var(--text-primary);
-        padding: 1rem;
-    }
-    :global(.upload-icon) {
-        color: var(--text-primary);
-        margin-top: 3rem;
-    }
+	.container {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		height: 100vh;
+		width: 100%;
+	}
+	/* h1 {
+		color: var(--text-primary);
+		margin-top: 5rem;
+		font-size: 3rem;
+	} */
+	.file-upload {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		height: 15rem;
+		width: 25rem;
+		background-color: var(--gray);
+		box-shadow:
+			rgba(0, 0, 0, 0.16) 0px 10px 36px 0px,
+			rgba(0, 0, 0, 0.06) 0px 0px 0px 1px;
+		border-radius: 16px;
+		margin-top: 5rem;
+		border: 2px dashed var(--text-primary);
+		text-align: center;
+	}
+	p {
+		color: var(--text-primary);
+		padding: 1rem;
+		font-size: 1.1rem;
+	}
+	:global(.upload-icon) {
+		margin-top: 2rem;
+		color: var(--blue);
+		transition: 0.3s;
+	}
+	.choose-file-btn {
+		font-size: 1rem;
+		font-family: inherit;
+		border: 2px solid var(--blue);
+		border-radius: 8px;
+		padding: 0.5rem 1rem;
+		background-color: transparent;
+		color: var(--text-primary);
+		transition: 0.5s;
+	}
+	.choose-file-btn:hover {
+		background-color: var(--blue);
+		transition: 0.5s;
+		cursor: pointer;
+	}
+	.file-input {
+		display: none;
+	}
 </style>
