f58974041cf74d5c4b06007bfa5128e47e8d3f80
Eetu <99178022+E3TU@users.noreply.github.com>
2026-04-09 23:43:55 +0300

    Create appwrite client

---
+++ backend/internal/appwrite/client.go
@@ -0,0 +1,16 @@
+package appwrite
+
+import (
+	"file-manager/internal/config"
+
+	"github.com/appwrite/sdk-for-go/appwrite"
+	"github.com/appwrite/sdk-for-go/client"
+)
+
+func NewClient(cfg config.Config) client.Client {
+	return appwrite.NewClient(
+		appwrite.WithEndpoint(cfg.AppwriteEndpoint),
+		appwrite.WithProject(cfg.AppwriteProjectId),
+		appwrite.WithKey(cfg.AppwriteApiKey),
+	)
+}
