5f7cfe2f75a1b09b56119965c671e0010b60d7bc
Eetu <99178022+E3TU@users.noreply.github.com>
2026-04-09 23:44:16 +0300

    Storage

---
+++ backend/internal/http/handlers/storage.go
@@ -0,0 +1,8 @@
+// internal/http/handlers/storage.go
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) UploadFile(c *gin.Context) {}
+func (h *Handler) ListFiles(c *gin.Context)  {}
+func (h *Handler) DeleteFile(c *gin.Context) {}
