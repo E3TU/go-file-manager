7ef9ccb232079fe9ecd4dae0b9d908bb8a087a8c
Eetu <99178022+E3TU@users.noreply.github.com>
2026-04-12 22:24:40 +0300

    Delete files

--- backend/internal/http/handlers/auth.go
+++
@@ -1,7 +0,0 @@
-package handlers
-
-import "github.com/gin-gonic/gin"
-
-func (h *Handler) CreateSession(c *gin.Context) {}
-func (h *Handler) GetSession(c *gin.Context)    {}
-func (h *Handler) DeleteSession(c *gin.Context) {}
--- backend/internal/http/handlers/storage.go
+++
@@ -1,8 +0,0 @@
-// internal/http/handlers/storage.go
-package handlers
-
-import "github.com/gin-gonic/gin"
-
-func (h *Handler) UploadFile(c *gin.Context) {}
-func (h *Handler) ListFiles(c *gin.Context)  {}
-func (h *Handler) DeleteFile(c *gin.Context) {}
