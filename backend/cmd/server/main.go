0f415401d3014356007a84546b63bb961fa403ad
Eetu <99178022+E3TU@users.noreply.github.com>
2026-04-12 22:25:25 +0300

    Update main.go

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,25 +3,34 @@ package main
 import (
 	"file-manager/internal/appwrite"
 	"file-manager/internal/config"
-	router "file-manager/internal/http"
-	"file-manager/internal/http/handlers"
-	"file-manager/internal/services"
+	"file-manager/internal/handlers"
+	router "file-manager/internal/router"
+	appwriteSvc "file-manager/internal/services/appwrite"
 	"log"
+	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
 	cfg := config.Load()
+	log.Printf("Loaded endpoint: %s", cfg.AppwriteEndpoint)
+	log.Printf("Loaded project: %s", cfg.AppwriteProjectId)
+	log.Printf("Loaded api key: %s", cfg.AppwriteApiKey)
+	log.Printf("PORT env: %s", os.Getenv("PORT"))
 
 	client := appwrite.NewClient(cfg)
 
-	authSvc := services.NewAuthService(client)
-	storageSvc := services.NewStorageService(client, cfg.BucketID)
+	authSvc := appwriteSvc.NewAuthService(client, cfg)
 
-	h := handlers.NewHandler(authSvc, storageSvc)
+	h := handlers.NewHandler(authSvc)
 
 	r := gin.Default()
+	r.Use(cors.Default())
 	router.RegisterRoutes(r, h)
 
 	log.Fatal(r.Run(":" + cfg.Port))
